0b3381496bf3a32e39086018c77f6f8608c147f5
Herique <henriquedimarco.14@gmail.com>
2026-09-19 15:37:00 -0300

    Adição do sprite do personagem juntamente com a movimentação

--- The Betas/TheBetas.c
+++ The Betas/TheBetas.c
@@ -3,29 +3,37 @@
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_font.h>
 #include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_image.h>
+
+#define CELL_W 52
+#define CELL_H 96
+
+enum { DIR_BAIXO = 0, DIR_CIMA = 1, DIR_ESQUERDA = 2, DIR_DIREITA = 3 };
 
 int main() {
 
 	al_init();
 	al_init_font_addon();
 	al_init_primitives_addon();
+	al_init_image_addon();
 	al_install_keyboard();
 
-	ALLEGRO_DISPLAY* display = al_create_display(1280, 720);
+	ALLEGRO_DISPLAY* display = al_create_display(800, 600);
 	al_set_window_position(display, 200, 200);
 	al_set_window_title(display, "The Betas");
 	ALLEGRO_FONT* fonte = al_create_builtin_font();
+	ALLEGRO_BITMAP* personagem_sprites = al_load_bitmap("personagem_spritesheet.png");
 	ALLEGRO_TIMER* fps = al_create_timer(1.0 / 60.0);
 	ALLEGRO_EVENT_QUEUE* evento = al_create_event_queue();
 	al_register_event_source(evento, al_get_display_event_source(display));
 	al_register_event_source(evento, al_get_timer_event_source(fps));
 	al_start_timer(fps);
 
-	float personagem_x = 400 - 16;
-	float personagem_y = 300 - 24;
-	float personagem_largura = 32;
-	float personagem_altura = 48;
+	float personagem_x = 400 - CELL_W / 2;
+	float personagem_y = 300 - CELL_H / 2;
 	float velocidade = 3;
+	int direcao = DIR_BAIXO;
+	bool andando = false;
 
 	bool rodando = true;
 	bool redesenhando = true;
@@ -38,15 +46,33 @@ int main() {
 			ALLEGRO_KEYBOARD_STATE teclado;
 			al_get_keyboard_state(&teclado);
 
-			if (al_key_down(&teclado, ALLEGRO_KEY_RIGHT)) personagem_x += velocidade;
-			if (al_key_down(&teclado, ALLEGRO_KEY_LEFT)) personagem_x -= velocidade;
-			if (al_key_down(&teclado, ALLEGRO_KEY_UP)) personagem_y -= velocidade;
-			if (al_key_down(&teclado, ALLEGRO_KEY_DOWN)) personagem_y += velocidade;
+			andando = false;
+
+			if (al_key_down(&teclado, ALLEGRO_KEY_RIGHT)) {
+				personagem_x += velocidade;
+				direcao = DIR_DIREITA;
+				andando = true;
+			}
+			if (al_key_down(&teclado, ALLEGRO_KEY_LEFT)) {
+				personagem_x -= velocidade;
+				direcao = DIR_ESQUERDA;
+				andando = true;
+			}
+			if (al_key_down(&teclado, ALLEGRO_KEY_UP)) {
+				personagem_y -= velocidade;
+				direcao = DIR_CIMA;
+				andando = true;
+			}
+			if (al_key_down(&teclado, ALLEGRO_KEY_DOWN)) {
+				personagem_y += velocidade;
+				direcao = DIR_BAIXO;
+				andando = true;
+			}
 
 			if (personagem_x < 0) personagem_x = 0;
 			if (personagem_y < 0) personagem_y = 0;
-			if (personagem_x + personagem_largura > 800) personagem_x = 800 - personagem_largura;
-			if (personagem_y + personagem_altura > 600) personagem_y = 600 - personagem_altura;
+			if (personagem_x + CELL_W > 800) personagem_x = 800 - CELL_W;
+			if (personagem_y + CELL_H > 600) personagem_y = 600 - CELL_H;
 
 			redesenhando = true;
 		}
@@ -59,13 +85,13 @@ int main() {
 
 			al_clear_to_color(al_map_rgb(0, 0, 0));
 
-
-			al_draw_filled_rectangle(personagem_x, personagem_y, personagem_x + personagem_largura, personagem_y + personagem_altura, al_map_rgb(80, 160, 220));
+			al_draw_bitmap_region(personagem_sprites, direcao * CELL_W, (andando ? 1 : 0) * CELL_H, CELL_W, CELL_H, personagem_x, personagem_y, 0);
 
 			al_flip_display();
 		}
 	}
 
+	al_destroy_bitmap(personagem_sprites);
 	al_destroy_font(fonte);
 	al_destroy_timer(fps);
 	al_destroy_display(display);
